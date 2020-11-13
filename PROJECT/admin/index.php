b496676d5aff33edcc6547bbffc1aae4e4cfb960
white-hat-256 <1851171980@e.tlu.edu.vn>
2020-11-13 09:41:22 +0700

    add index admin

---
+++ PROJECT/admin/index.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet"href="https://fonts.googleapis.com/css?family=Tangerine">
+    <link rel="stylesheet"  href="style.css">  
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+
+      <header >
+            <h3>Admin</h3>
+            <div class="nav">
+                <div class="nav-input">
+                    <input class="navbar-search-input" type="text" name="search" placeholder="Search">
+                    <button >
+                        Search
+                    </button>
+                </div>
+                <div class="nav-item">
+                    <a href="" class="nav-link">
+                    <i class="fas fa-bell"></i>
+                    <i class="fas fa-envelope"></i>
+                    </a>
+                </div>
+            </div>
+      </header>
+<br>
+<br>
+<br>
+      <div class="sidebar">
+          <ul class="sidebar-nav">
+            <li class="sidebar-nav-item">
+              <a href="" class="sidebar-nav-link">
+                <div>
+                  <i class="fas fa-tachometer-alt"></i>
+                </div>
+                <span>Dashboard</span>
+
+              </a>
+            </li>
+            <li class="sidebar-nav-item">
+              <a href="" class="sidebar-nav-link">
+                <div>
+                <i class="fas fa-book-open"></i>
+                </div>
+                <span>Quản lý hồ sơ</span>
+
+              </a>
+            </li>
+            <li class="sidebar-nav-item">
+              <a href="" class="sidebar-nav-link">
+                <div>
+                <i class="fas fa-tasks"></i>
+                </div>
+                <span>Quản lý sinh viên</span>
+
+              </a>
+            </li>
+            <li class="sidebar-nav-item">
+              <a href="" class="sidebar-nav-link">
+                <div>
+                <i class="far fa-newspaper"></i>
+                </div>
+                <span>Quản lý tin tức</span>
+
+              </a>
+            </li>
+            
+            <li class="sidebar-nav-item">
+              <a href="" class="sidebar-nav-link">
+                <div>
+                <i class="fas fa-user-cog"></i>
+                </div>
+                <span>Cài đặt</span>
+              </a>
+            </li>
+
+            <li class="sidebar-nav-item">
+              <a href="" class="sidebar-nav-link">
+                <div>
+                <i class="fas fa-sign-out-alt"></i>
+                </div>
+                <span>Đăng xuất</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
